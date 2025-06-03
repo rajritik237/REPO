6c9df40ab83b4f75214177d75824a9c6be279b88
ritik <ritik@Ritik_Dell>
2025-06-03 17:54:50 +0530

    String functions

---
+++ #string functions.py
@@ -0,0 +1,9 @@
+#string functions 
+hi='Hi i am a coder'
+'''hi.endswith('er') # returns true if string ends with substring
+hi.capitalize() #capitalise first caracter
+hi.replace('Hi','yes') */#replace all occurence Hi with yes
+text=hi.find('am')
+print(text)'''
+text2=hi.count('i')
+print(text2)
